976e38739e2ac2f4890c0deede13400bd2f3e687
Mortadha Alaa <mortadha.alaa@gmail.com>
2017-11-04 13:20:13 +0300

    Removed unnecessary comment

--- bot.py
+++ bot.py
@@ -1,19 +1,5 @@
 # -*- coding: utf-8 -*-
 import os
-# import some_api_lib
-# import ...
-
-# Example of your code beginning
-#           Config vars
-# token = os.environ['TELEGRAM_TOKEN']
-#some_api_token = os.environ['SOME_API_TOKEN']
-#             ...
-
-# If you use redis, install this add-on https://elements.heroku.com/addons/heroku-redis
-#       Your bot code below
-# bot = telebot.TeleBot(token)
-# some_api = some_api_lib.connect(some_api_token)
-#              ...
 import logging
 from telegram import (InlineKeyboardButton, InlineKeyboardMarkup,
                                       InlineQueryResultArticle, InputTextMessageContent)
